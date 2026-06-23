34e3cc206d00659c48fd1da4c92da830fbacbb13
Benicio <benicio@Benicios-MacBook-Pro.local>
2026-06-23 16:13:46 -0500

    added basic python logger

---
+++ python-reader/serialLogger.py
@@ -0,0 +1,33 @@
+"""
+
+This is just a rudementary serial data logger using the pyubx2 library
+
+"""
+
+from serial import Serial
+from pyubx2 import UBXReader
+from pathlib import Path
+import datetime
+
+# initalize the folder for data according to the time and day
+
+now = datetime.now().strftime("%m%d%H%M")
+
+dir_path = Path.home() / "documents" / "gps" / "recordings" / now
+dir_path.mkdir(parents=True, exist_ok=True)
+log_file_path = dir_path / "gnss_log.ubx"
+
+print(f"Logging to {log_file_path}")
+
+# opening file stream to write in binary
+
+with open(log_file_path, "wb") as f_out:
+    with Serial('dev/serial0', 115200, timeout=1) as stream:
+        ubr = UBXReader(stream) # initializing the ubx stream
+        for (raw_data, parsed_data) in ubr:
+            if raw_data:
+                f_out.write(raw_data)
+                f_out.flush()
+            if parsed_data:
+                print(parsed_data)
+        
