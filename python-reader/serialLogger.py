633f8a9a64d037c6cf674a42ffacc1dfa4cb143d
Benicio <benicio@Benicios-MacBook-Pro.local>
2026-06-23 22:46:18 -0500

    fixed path

--- python-reader/serialLogger.py
+++ python-reader/serialLogger.py
@@ -13,7 +13,7 @@ import datetime
 
 now = datetime.now().strftime("%m%d%H%M")
 
-dir_path = Path.home() / "documents" / "gps" / "recordings" / now
+dir_path = Path.home() / "Documents" / "gps" / "recordings" / now
 dir_path.mkdir(parents=True, exist_ok=True)
 log_file_path = dir_path / "gnss_log.ubx"
 
@@ -22,12 +22,11 @@ print(f"Logging to {log_file_path}")
 # opening file stream to write in binary
 
 with open(log_file_path, "wb") as f_out:
-    with Serial('dev/serial0', 115200, timeout=1) as stream:
-        ubr = UBXReader(stream) # initializing the ubx stream
+    with Serial('/dev/serial0', 115200, timeout=1) as stream:
+        ubr = UBXReader(stream)     # initializing the ubx stream
         for (raw_data, parsed_data) in ubr:
             if raw_data:
                 f_out.write(raw_data)
                 f_out.flush()
             if parsed_data:
                 print(parsed_data)
-        
